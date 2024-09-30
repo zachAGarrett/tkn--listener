2a5c30c131893937431c179c2c86632beafc9cd2
zachagarrett <zach.garrett.archery@gmail.com>
2024-09-30 15:04:26 -0400

    improved logs, extracted logic

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { ingestString, TokenBank } from "./lib/ingest/string/index.js";
 import { getWikipediaArticle } from "./lib/sources/wikipedia/index.js";
+import { calculateCycleGrowth, mean, profileTokenBank } from "./util/math.js";
 
 const timers = ["Retrieved article in", "Ingested article in"];
 
@@ -8,31 +9,24 @@ const textContent = await getWikipediaArticle("Operator_algebra");
 console.timeEnd(timers[0]);
 
 const totalCycles = 100;
-const growthCutoff = 2;
+const growthCutoff = 4;
 let cycle = totalCycles;
 let persistentBank: TokenBank = {};
 let growthOverCycle: number = Infinity;
+let profile: ReturnType<typeof profileTokenBank> | undefined = undefined;
 
 while (cycle > 0 && growthOverCycle > growthCutoff) {
   console.time(timers[1]);
   const results = await ingestString(textContent, persistentBank);
   console.timeEnd(timers[1]);
 
-  const sortedBank = Object.entries(results)
-    .sort((a, b) => b[1].length - a[1].length)
-    .map(([value, instances]) => [value, instances.length + " instances"]);
-
-  const previouslyKnownTokenSetSize = Object.keys(persistentBank).length;
-  const resultingTokenSetSize = sortedBank.length;
-  growthOverCycle =
-    ((resultingTokenSetSize - previouslyKnownTokenSetSize) /
-      previouslyKnownTokenSetSize) *
-    100;
+  profile = profileTokenBank(results);
+  growthOverCycle = calculateCycleGrowth(persistentBank, results);
 
   console.log(
     `Results after cycle ${totalCycles - cycle}:`,
-    `Tokens in bank: ${resultingTokenSetSize}`,
-    `Growth over cycle: ${Math.round(growthOverCycle)}%`
+    `Growth over cycle: ${Math.round(growthOverCycle)}%`,
+    JSON.stringify(profile, undefined, 2)
   );
 
   persistentBank = results;
@@ -40,3 +34,12 @@ while (cycle > 0 && growthOverCycle > growthCutoff) {
 }
 
 console.log(`Elapsed cycles: ${totalCycles - cycle}`);
+
+// const sortedBank = Object.entries(persistentBank)
+//   .sort((a, b) => b[1].length - a[1].length)
+//   .map(([value, instances]) => [value, instances.length + " instances"]);
+
+// console.log(
+//   sortedBank.slice(0, 1000),
+//   `Token Bank Profile: ${JSON.stringify(profile, undefined, 2)}`
+// );
