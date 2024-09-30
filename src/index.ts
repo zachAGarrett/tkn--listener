541f73d22b0c79aeceb997e1216af42a3e6a401a
zachagarrett <zach.garrett.archery@gmail.com>
2024-09-30 14:37:23 -0400

    added cycle cutoff

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
 import { ingestString, TokenBank } from "./lib/ingest/string/index.js";
 import { getWikipediaArticle } from "./lib/sources/wikipedia/index.js";
 
-const timers = ["Retrieved article", "Ingested article"];
+const timers = ["Retrieved article in", "Ingested article in"];
 
 console.time(timers[0]);
 const textContent = await getWikipediaArticle("Operator_algebra");
 console.timeEnd(timers[0]);
 
-const totalCycles = 20;
+const totalCycles = 100;
+const growthCutoff = 2;
 let cycle = totalCycles;
 let persistentBank: TokenBank = {};
+let growthOverCycle: number = Infinity;
 
-while (cycle > 0) {
+while (cycle > 0 && growthOverCycle > growthCutoff) {
   console.time(timers[1]);
   const results = await ingestString(textContent, persistentBank);
   console.timeEnd(timers[1]);
@@ -22,18 +24,19 @@ while (cycle > 0) {
 
   const previouslyKnownTokenSetSize = Object.keys(persistentBank).length;
   const resultingTokenSetSize = sortedBank.length;
+  growthOverCycle =
+    ((resultingTokenSetSize - previouslyKnownTokenSetSize) /
+      previouslyKnownTokenSetSize) *
+    100;
 
   console.log(
     `Results after cycle ${totalCycles - cycle}:`,
-    sortedBank.slice(0, 10),
     `Tokens in bank: ${resultingTokenSetSize}`,
-    `Growth over cycle: ${Math.round(
-      ((resultingTokenSetSize - previouslyKnownTokenSetSize) /
-        previouslyKnownTokenSetSize) *
-        100
-    )}%`
+    `Growth over cycle: ${Math.round(growthOverCycle)}%`
   );
 
   persistentBank = results;
   cycle = cycle - 1;
 }
+
+console.log(`Elapsed cycles: ${totalCycles - cycle}`);
